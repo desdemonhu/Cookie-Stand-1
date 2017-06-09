2217e8ee26a82a6e69d63f0743418f9b939d063f
Timothy Cullen <timothycullen@Timothys-MacBook-Pro.local>
2017-06-08 20:29:44 -0700

    will refactor

---
+++ app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+   var pike = {
+     var minCust:23,
+     var maxCust:65,
+     avgCkSl:6.3
+   };
+
+   var seaTac = {
+     var minCust:3,
+     var maxCust:24,
+     avgCkSl:1.2
+   };
+
+   var seaCenter= {
+     var minCust:11,
+     var maxCust:38,
+     avgCkSl:3.7
+   };
+
+   var capHill = {
+     var minCust:20,
+     var maxCust:38,
+     avgCkSl:2.3
+   };
+
+   var alki = {
+     var minCust:2,
+     var maxCust:16,
+     avgCkSl:4.6
+   };
