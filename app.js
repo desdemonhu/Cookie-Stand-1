6b557620a2aefdb22404a427bb0af084fdb7e758
Timothy Cullen <timothycullen@Timothys-MacBook-Pro.local>
2017-06-16 20:12:32 -0700

    working through js

--- app.js
+++ app.js
@@ -1,47 +1,68 @@
 'use strict';
-// // need to use object constructors to replace literals for 5! stores
-// //then dynamically build html page using selectors
-// //containing data for hourly sales at each location
-// //reference book and lecture on youtube for review
-//
-// // //object constructors
-function Location(shop,hourlysales,dailytotal) {
-  this.location = shop;
-  this.hourlySales = hourlysales;
-  this.dailyTotal = dailytotal;
-}
+// dclate some mathy stuff for the properties?
 
-//use an array for hours?
-//function to push hours onto table?
-//hard code hours to HTML?  more better;
-function makeRow(Location) {
-  //create element
+function CookieShop(hourlySales,dailySales) {
+  this.location = location;
+  this.dailySales = dailySales;
+  this.hourlySales = hourlySales;
+}
+//this is constructor function now we set up the table
+ //Step 1 - create element
+function makeRow(cookieShop) {
   var newRowEl = document.createElement('tr');
-  var locationDataEl = document.createElement('tr');
-  var hourlySalesEl = document.createElement('tr');
-  var dailyTotalEL = document.createElement ('tr');
-  //Configure "mutate"
-  locationDataEl.textContent = pike.shop;
-  hourlySalesEl.textContent = pike.hourlysales;
-  dailyTotalEL.textContent = pike.dailytotal;
-  //append to child
+    //Step 2 - Configure/Mutate the ROW element
+  var cookieShopDataEl = document.createElement('td');
+  cookieShopDataEl.textContent = cookieshop.location;
+  newRowEl.appendChild(cookieShopDataEl);
+
+  var dailySales = document.createElement('td');
+  dailySalesDataEl.textContent = cookieShop.dailySales;
+  newRowEl.appendChild(dailySalesDataEl);
+
+  var hourlySales = document.createElement('td');
+  hourlySalesDataEl.textContent = cookieShop.hourlySales;
+  newRowEl.appendChild(hourlySalesDataEl);
+
+  //Step 3 Append to the document
+
   tableBodyEl.appendChild(newRowEl);
 }
 
-var alki = new Location('alki','hourlysales','dailytotal');
-var pike = new Location('pike','hourlysales','totalsales');
-var seacenter = new Location('seacenter','hourlysales','totalsales');
-var caphill = new Location('caphill','hourlysales','totalsales');
-var seatac = new Location('seatac','hourlysales','totalsales');
-// console.log(alki);
-// console.log(pike);
-// console.log(seacenter); // all of these console's were successfull
-// console.log(caphill);
-// console.log(seatac);
+function test( test) {
+  console.log('it lives!');
+  console.log('but will it dynamically render?');
+}
 
-var tableEl = document.getElementById('dynamic-table');
-var newRowEl = document.createElement('tr');
-var tableBodyEl = document.createElement('tbody');
-tableEl.appendChild(tableBodyEl);
+test();
+var pike = new cookieShop('Pike', hourlySalesPike, dailySalesPike);
+var alki = new cookieShop('Alki', hourlySalesAlki,dailySalesAlki);
+var SeaCenter = new cookieShop('SeaCenter', hourlySalesSeacenter,dailySalesSeaCenter);
+var capHill = new cookieShop('capHill', hourlySalesCapHill,dailySalesCapHill);
+var seaTac = new cookieShop('seaTac', hourlySalesseaTac,dailySalesseaTac);
 
-makeRow(pike); //If I get it correctly each function call requires specifying the name of the Variable in the configuration(?)
+var hourlySalesPike;
+var dailySalesPike;
+var hourlySalesseaTac;
+var dailySalesseaTac;
+var hourlySalesCapHill;
+var dailySalesCapHill;
+var hourlySalesSeacenter;
+var dailySalesSeaCenter;
+
+// var alki = new Location('alki','hourlysales','dailytotal');
+// var pike = new Location('pike','hourlysales','totalsales');
+// var seacenter = new Location('seacenter','hourlysales','totalsales');
+// var caphill = new Location('caphill','hourlysales','totalsales');
+// var seatac = new Location('seatac','hourlysales','totalsales');
+// // console.log(alki);
+// // console.log(pike);
+// // console.log(seacenter); // all of these console's were successfull
+// // console.log(caphill);
+// // console.log(seatac);
+//
+// var tableEl = document.getElementById('dynamic-table');
+// var newRowEl = document.createElement('tr');
+// var tableBodyEl = document.createElement('tbody');
+// tableEl.appendChild(tableBodyEl);
+//
+// makeRow(pike); //If I get it correctly each function call requires specifying the name of the Variable in the configuration(?)
